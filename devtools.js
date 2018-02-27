4e2e0dcc3d35d3374a8b1ce0eaa0b589ec78f4f1
Robert-Fairley <robert.fairley@rci.rogers.com>
2018-02-27 16:46:10 -0500

    init

---
+++ devtools.js
@@ -0,0 +1,32 @@
+const script = `
+function DevTools(opts = {}) {
+	// Check to see if things are working right. Being that this is recursive it's a little redundant.
+	// You can probably do better.
+	window.testFunc = function() {
+		if (!window.DevTools) {
+			console.error("Dev Tools is not working properly. Check your installation.");
+			return 1;
+		}
+		console.log("Dev Tools loaded correctly.");
+	};
+
+	// Sample Functions. You might want to modify these.
+	window.checkClicks = function() {
+		monitorEvents(document.body, "click");
+	};
+
+	window.stopCheck = function() {
+		unmonitorEvents(document.body);
+	}
+}
+
+DevTools();
+
+`;
+
+let sElement = document.createElement('script');
+sElement.innerHTML = script;
+document.querySelector('head').appendChild(sElement);
+
+
+
